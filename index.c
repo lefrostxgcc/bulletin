7372ce2358776bc2f9607b655a1afca221252e22
Chip <lefrostx@gmail.com>
2019-12-27 13:09:19 +0500

    Added test for QUERY_STRING env variable

--- index.c
+++ index.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
+  const char *user = getenv("QUERY_STRING");
   printf("Content-Type: text/html\n\n");
-  printf(
+  printf("%s %s %s",
 	 "<!DOCTYPE html>"
 	 "<html>"
 	 "<style type=\"text/css\">"
 	 ".centerDiv"
 	 "{"
-	 "position: fixed; top: 50%%; left: 50%%;"
-	 "transform: translate(-50%%, -50%%);"
+	 "position: fixed; top: 50%; left: 50%;"
+	 "transform: translate(-50%, -50%);"
 	 "color: #000080;"
 	 "}"
 	 "</style>"
 	 "<body>"
 	 "<div class=\"centerDiv\">"
 	 "<h1>Hello Videosharp Web Bulletin Board!</h1>"
+	 "<h1>",
+	 user,
+	 "<h1>"
 	 "</div>"
 	 "</body>"
 	 "</html>"
