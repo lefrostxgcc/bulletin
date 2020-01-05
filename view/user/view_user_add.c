a0f2726d3035f292ac7666ec323ec073376f1cda
Chip <lefrostx@gmail.com>
2020-01-05 18:31:01 +0500

    Add Login Password Confirm in UserForm

--- view/user/view_user_add.c
+++ view/user/view_user_add.c
@@ -2,49 +2,32 @@
 #include "view_user.h"
 #include "../read_replace_write.h"
 #include "../../model/model_user.h"
-
-static inline void render_user_add_as_guest(void)
-{
-  const struct Key_value map[] =
-    {
-     (const struct Key_value){.key = "LOGIN_HREF", .value = "/site/login"},
-     (const struct Key_value){.key = "LOGIN", .value = "Вход"},
-     (const struct Key_value)
-     {
-      .key = "CONTENT",
-      .value = "<h3>Новый пользователь</h3>"
-     },
-     (const struct Key_value){.key = NULL, .value = NULL}
-    };
-  read_replace_write("htmlt/user_add.html", map, NULL);
-}
-
-static inline void render_user_add_as_user(const struct Model_user *user)
-{
-  char login_caption[320];
-  snprintf(login_caption,
-	   sizeof(login_caption) / sizeof(login_caption[0]),
-	   "Выход(%s)",
-	   user->username
-	   );
-  const struct Key_value map[] =
-    {
-     (const struct Key_value){.key = "LOGIN_HREF", .value = "/site/logout"},
-     (const struct Key_value){.key = "LOGIN", .value = login_caption},
-     (const struct Key_value)
-     {
-      .key = "CONTENT",
-      .value = "<h3>Новый пользователь</h3>"
-     },
-     (const struct Key_value){.key = NULL, .value = NULL}
-    };
-  read_replace_write("htmlt/user_add.html", map, NULL);
-}
+#include "../../model/model_userform.h"
 
 void render_user_add(const struct Model_user *user)
 {
+  const char **labels = model_userform_attribute_labels();
+  const char *login_caption = "Вход";
+  const char *login_href = "/site/login";
+  char buf[320];
   if (user)
-    render_user_add_as_user(user);
-  else
-    render_user_add_as_guest();
+    {
+      snprintf(buf,
+	       sizeof(buf) / sizeof(buf[0]),
+	       "Выход(%s)",
+	       user->username
+	       );
+      login_caption = buf;
+      login_href = "/site/logout";
+    }
+  const struct Key_value map[] =
+    {
+     {.key = "LOGIN_HREF", .value = login_href},
+     {.key = "LOGIN", .value = login_caption},
+     {.key = "USERFORM_LOGIN_LABEL", .value = labels[USERFORM_USERNAME_INDEX]},
+     {.key = "USERFORM_PASSWORD_LABEL", .value = "Пароль"},
+     {.key = "USERFORM_CONFIRM_PASSWORD_LABEL", .value = "Подтвердить пароль"},
+     {.key = NULL, .value = NULL}
+    };
+  read_replace_write("htmlt/user_add.html", map, NULL);
 }
--- view/user/view_user_add.d
+++ view/user/view_user_add.d
@@ -1,3 +1,4 @@
 view/user/view_user_add.o view/user/view_user_add.d : view/user/view_user_add.c view/user/view_user.h \
  view/user/../../model/model_user.h \
- view/user/../../model/model_userinfo.h view/user/../read_replace_write.h
+ view/user/../../model/model_userinfo.h view/user/../read_replace_write.h \
+ view/user/../../model/model_userform.h
