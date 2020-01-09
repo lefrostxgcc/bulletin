35a18c5a0132be425f677ba6e65c04021fffa56d
Chip <lefrostx@gmail.com>
2020-01-09 14:17:12 +0500

    Add user info menu item for log user

--- view/user/view_user_index.c
+++ view/user/view_user_index.c
@@ -1,47 +1,18 @@
-#include <stdio.h>
+#include <stddef.h>
 #include "view_user.h"
 #include "../read_replace_write.h"
 #include "../../model/model_user.h"
 #include "../../model/model_userinfo.h"
 
-static inline void render_user_index_guest(void)
+void render_user_index(const struct Model_user *user,
+		       const struct Model_userinfo *userinfo)
 {
   const struct Key_value map[] =
     {
-     (const struct Key_value){.key = "LOGIN_HREF", .value = "/site/login"},
-     (const struct Key_value){.key = "LOGIN", .value = "Вход"},
-     (const struct Key_value){.key = "USERNAME", ""},
-     (const struct Key_value){.key = "SURNAME", ""},
-     (const struct Key_value){.key = NULL, .value = NULL}
-    };
-  read_replace_write("htmlt/user_index.html", map, NULL);
-}
-
-static inline void render_user_index_user(const struct Model_user *user,
-					  const struct Model_userinfo *userinfo)
-{
-  char login_caption[320];
-  snprintf(login_caption,
-	   sizeof(login_caption) / sizeof(login_caption[0]),
-	   "Выход(%s)",
-	   user->username
-	   );
-  const struct Key_value map[] =
-    {
-     (const struct Key_value){.key = "LOGIN_HREF", .value = "/site/logout"},
-     (const struct Key_value){.key = "LOGIN", .value = login_caption},
+     (const struct Key_value){.key = "LOGIN", .value = user->username},
      (const struct Key_value){.key = "USERNAME", .value = user->username},
      (const struct Key_value){.key = "SURNAME", .value = userinfo->surname},
      (const struct Key_value){.key = NULL, .value = NULL}
     };
   read_replace_write("htmlt/user_index.html", map, NULL);
-}
-
-void render_user_index(const struct Model_user *user,
-		       const struct Model_userinfo *userinfo)
-{
-  if (user && userinfo)
-    render_user_index_user(user, userinfo);
-  else
-    render_user_index_guest();
 }
