89bb9b01ebf8788725d08a6afb51dcf6bccb0e53
Chip <lefrostx@gmail.com>
2020-01-14 14:02:04 +0500

    Add read labels from model

--- view/bulletins/view_bulletins_addbulletin.c
+++ view/bulletins/view_bulletins_addbulletin.c
@@ -1,11 +1,33 @@
 #include <stddef.h>
 #include "view_bulletins.h"
+#include "../../model/model_bulletinsform.h"
 #include "../read_replace_write.h"
 
 void render_bulletins_addbulletin(const char *username)
 {
+  const char **labels = model_bulletinsform_attribute_labels();
   const struct Key_value map[] =
     {
+     {
+      .key = "BULLETINSFORM_TITLE_LABEL",
+      .value = labels[BULLETINSFORM_TITLE_INDEX]
+     },
+     {
+      .key = "BULLETINSFORM_INFO_LABEL",
+      .value = labels[BULLETINSFORM_INFO_INDEX]
+     },
+     {
+      .key = "BULLETINSFORM_CONTACTS_LABEL",
+      .value = labels[BULLETINSFORM_CONTACTS_INDEX]
+     },
+     {
+      .key = "BULLETINSFORM_CITY_LABEL",
+      .value = labels[BULLETINSFORM_CITY_INDEX]
+     },
+     {
+      .key = "BULLETINSFORM_PRICE_LABEL",
+      .value = labels[BULLETINSFORM_PRICE_INDEX]
+     },
      {.key = "LOGIN", .value = username},
      {.key = NULL, .value = NULL}
     };
--- view/bulletins/view_bulletins_addbulletin.d
+++ view/bulletins/view_bulletins_addbulletin.d
@@ -1,2 +1,4 @@
 view/bulletins/view_bulletins_addbulletin.o view/bulletins/view_bulletins_addbulletin.d : view/bulletins/view_bulletins_addbulletin.c \
- view/bulletins/view_bulletins.h view/bulletins/../read_replace_write.h
+ view/bulletins/view_bulletins.h \
+ view/bulletins/../../model/model_bulletinsform.h \
+ view/bulletins/../read_replace_write.h
